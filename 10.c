a5be626210284fd2d44b27b6f85dbd8baaec6a5a
VAMSI-gif948 <122437323+VAMSI-gif948@users.noreply.github.com>
2023-02-02 21:09:10 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+int age;
+{
+printf("Enter the age:");
+}
+scanf("%d",&age);
+if(age>=18)
+printf("you can vote");
+else
+printf("you can vote after %d years",18-age);
+return 'o';
+}
