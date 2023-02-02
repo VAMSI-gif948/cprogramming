9478695cbabda7503220188145f355c93fd42137
VAMSI-gif948 <122437323+VAMSI-gif948@users.noreply.github.com>
2023-02-02 21:12:43 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the value:");
+scanf("%d",&num);
+if(num%2==0)
+printf("the given number is even");
+else
+printf("the given number is odd");
+}
