d4653d33b731c83e01ecbccf5464dcc26cd19aa1
VAMSI-gif948 <122437323+VAMSI-gif948@users.noreply.github.com>
2023-02-02 21:19:42 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+int p,t,r,si;
+
+printf("enter the p value:");
+scanf("%d",&p);
+
+printf("enter the t value:");
+scanf("%d",&t);
+
+printf("enter the r value:");
+scanf("%d",&r);
+
+si=p*t*r/100;
+printf("the simple interest is :%d",si);
+return 'o';
+}
